8a4f4e7a102266c6b2a3327ad308af92afbc1648
Tiancai Ye <tiancai.ye@outlook.com>
2017-01-13 14:07:11 +0800

    11

--- App.py
+++ App.py
@@ -6,26 +6,33 @@ from PySide.QtGui import *
 
 
 class SystemTrayMenu(QMenu):
-    def __init__(self):
-        super(SystemTrayMenu, self).__init__()
-        self.addAction('quit').triggered.connect(self.quitApp)
+	def __init__(self, *args, **kwargs):
+		super(SystemTrayMenu, self).__init__(*args, **kwargs)
+		self.addAction('quit').triggered.connect(self.quitApp)
 
-    def quitApp(self):
-        qApp.quit()
+	def quitApp(self):
+		qApp.quit()
 
 
 class AppSystemTray(QSystemTrayIcon):
-    def __init__(self, *args, **kwargs):
-        super(AppSystemTray, self).__init__(*args, **kwargs)
-        self.setIcon(QIcon('icon.ico'))
-        self.setContextMenu(SystemTrayMenu())
+	def __init__(self, *args, **kwargs):
+		super(AppSystemTray, self).__init__(*args, **kwargs)
+		self.setIcon(QIcon('app.ico'))
+		self.setContextMenu(SystemTrayMenu())
+
+
+class App(QApplication):
+	def __init__(self, *args, **kwargs):
+		super(App, self).__init__(*args, **kwargs)
+
+	def winEventFilter(self, *args, **kwargs):
+		pass
 
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
+	app = QApplication(sys.argv)
 
-    tray = AppSystemTray()
-    tray.show()
+	tray = AppSystemTray()
+	tray.show()
 
-    app.exec_()
-
+	app.exec_()
